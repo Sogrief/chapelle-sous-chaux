d354c58cf5ba839afe668ea9f9163723382570df
Sogrief <CharlesJD@outlook.fr>
2023-01-30 18:57:46 +0100

    affichage actus page d'accueil
    boucle wordpress affichage des actus dans frontpage.php

--- front-page.php
+++ front-page.php
@@ -1,3 +1,30 @@
 <?php get_header(); ?>
-	<h1>accueil</h1>
+	<div class="bandeau -accueil">
+		<?php the_post_thumbnail(); ?>
+		<h1 class="titreH1 -blanc"><?php the_title(); ?></h1>
+
+		<h2><?php echo get_bloginfo( 'description' ); ?></h2>
+	</div>
+
+<?php 
+$args = array(
+	'post_type' => 'actus',
+	'posts_per_page' => 3
+);
+$actus = new WP_Query( $args );
+if ( $actus->have_posts() ) :
+	while ( $actus->have_posts() ) : $actus->the_post();?>
+
+	<h3><?php the_title(); ?></h3>
+	<?php the_excerpt(); ?>
+
+	<button class="wp-block-button">
+		<a href="<?php the_permalink(); ?>" class="post__link">lire la suite</a>
+	</button>
+<?php 
+	endwhile;
+	wp_reset_postdata();
+endif; 
+?>
+	
 <?php get_footer(); ?>
--- single-actus.php
+++ single-actus.php
@@ -1,5 +1,15 @@
 <?php get_header(); ?>
 
-<h1>actu</h1>
+<article class="actualite">
+
+<h1><?php the_title(); ?></h1>
+
+<?php the_post_thumbnail(); ?>
+
+    <div class="actualite__content">
+        <?php the_content() ?>
+    </div>
+
+</article>
 
 <?php get_footer(); ?>
