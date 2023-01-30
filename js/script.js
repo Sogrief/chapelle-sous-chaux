e3e6567b80f6eb8991fbdd2d9d3fa04d9709cf3a
Sogrief <CharlesJD@outlook.fr>
2023-01-30 15:07:02 +0100

    interraction menu
    déploiement des sous rubriques du menu au hover

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log("lee");
+/*-------------------------menu hamburger-------------------------*/
+
+let menu=document.getElementsByClassName("header")[0];//récupère le menu
+
+/*-------------------------sous rubriques-------------------------*/
+
+let rubriquesDeroulantes=menu.getElementsByClassName("site__header__menu")[0].getElementsByClassName("menu-item-has-children");
+
+for(i=0;i<rubriquesDeroulantes.length;i++)
+{
+    rubriquesDeroulantes[i].querySelector("a").href = "javascript:void(0)";//désactive les rubriques déroulantes
+    rubriquesDeroulantes[i].addEventListener("mouseover",function(){
+        this.getElementsByClassName("sub-menu")[0].style.display="block";
+    });
+
+    rubriquesDeroulantes[i].addEventListener("mouseout",function(){
+        this.getElementsByClassName("sub-menu")[0].style.display="none";
+    });
+}
