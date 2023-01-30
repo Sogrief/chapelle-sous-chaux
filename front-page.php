b78d46e17d5220dea99b1d01711a54337075f1a3
Sogrief <CharlesJD@outlook.fr>
2023-01-30 19:42:24 +0100

    actualités style
    ajout de style pour les actualités afin de les mettre en page sous forme de cartes

--- front-page.php
+++ front-page.php
@@ -6,25 +6,38 @@
 		<h2><?php echo get_bloginfo( 'description' ); ?></h2>
 	</div>
 
+	<h2>actualités</h2>
+
+	<div class="actusAccueil">
+
 <?php 
 $args = array(
 	'post_type' => 'actus',
 	'posts_per_page' => 3
 );
+
 $actus = new WP_Query( $args );
 if ( $actus->have_posts() ) :
 	while ( $actus->have_posts() ) : $actus->the_post();?>
+	<article class="actusAccueil__item">
 
-	<h3><?php the_title(); ?></h3>
-	<?php the_excerpt(); ?>
+		<?php the_post_thumbnail(); ?>
 
-	<button class="wp-block-button">
-		<a href="<?php the_permalink(); ?>" class="post__link">lire la suite</a>
-	</button>
+		<div class="actusAccueil__item__content">
+			<h3><?php the_title(); ?></h3>
+			<p><?php the_excerpt(); ?></p>
+
+			<button class="wp-block-button">
+				<a href="<?php the_permalink(); ?>" class="post__link">lire la suite</a>
+			</button>
+		</div>
+	</article>
 <?php 
 	endwhile;
 	wp_reset_postdata();
 endif; 
 ?>
+
+</div>
 	
 <?php get_footer(); ?>
