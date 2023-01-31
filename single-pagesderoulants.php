a32a9ed1fa35d89ebb2a5070c7f7154d9ad3044d
Sogrief <CharlesJD@outlook.fr>
2023-01-31 11:18:17 +0100

    deroulant style & responsive
    ajout display : grid sur les déroulants pour une mise en page 2 par 2

--- single-pagesderoulants.php
+++ single-pagesderoulants.php
@@ -6,8 +6,51 @@
 
 <div class="deroulants">
     <div class="deroulants__item">
+        <div class="deroulants__item__headband">
+            <div class="texteTitre">texte</div>
+            <img src="sources/icones/arrow.svg" alt="flèche">
+        </div>
+        <div class="deroulants__item__subContent">
+            <div class="texte">
+                col1
+            </div>
+        </div>
+    </div>
 
+    <div class="deroulants__item">
+        <div class="deroulants__item__headband">
+            <div class="texteTitre">texte</div>
+            <img src="sources/icones/arrow.svg" alt="flèche">
+        </div>
+        <div class="deroulants__item__subContent">
+            <div class="texte">
+                col1
+            </div>
+        </div>
+    </div>
 
+    <div class="deroulants__item">
+        <div class="deroulants__item__headband">
+            <div class="texteTitre">texte</div>
+            <img src="sources/icones/arrow.svg" alt="flèche">
+        </div>
+        <div class="deroulants__item__subContent">
+            <div class="texte">
+                col1
+            </div>
+        </div>
+    </div>
+    
+    <div class="deroulants__item">
+        <div class="deroulants__item__headband">
+            <div class="texteTitre">texte</div>
+            <img src="sources/icones/arrow.svg" alt="flèche">
+        </div>
+        <div class="deroulants__item__subContent">
+            <div class="texte">
+                col1
+            </div>
+        </div>
     </div>
 </div>
 
