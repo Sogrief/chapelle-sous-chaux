afc8d173d03e646cfccfe865d659edc6da70c618
Sogrief <CharlesJD@outlook.fr>
2023-01-31 11:06:58 +0100

    type de publication deroulant
    ajout du type de publication pour les démarches et création de single-slug_publication.php

---
+++ single-pagesderoulants.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<h1><?php the_title(); ?></h1>
+
+<?php the_content(); ?>
+
+<div class="deroulants">
+    <div class="deroulants__item">
+
+
+    </div>
+</div>
+
+<?php get_footer(); ?>
